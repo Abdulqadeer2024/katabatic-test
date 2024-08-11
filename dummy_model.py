0df805bf2b198628f39c767d7faf31ed2eacca3f
Abdulqadeer2024 <s223830288@deakin.edu.au>
2024-08-11 20:59:08 +1000

    Update dummy_model.py

--- dummy_model.py
+++ dummy_model.py
@@ -1,4 +1,5 @@
 from model import Model
+import unittest
 
 class DummyModel(Model):
     def __init__(self, x, Y, batch_size=64):
@@ -18,3 +19,13 @@ class DummyModel(Model):
 
     def evaluate(self):
         return 42
+
+def test_dummy_model():
+    model = DummyModel(None, None)
+    assert model.fit(None, None) == 42
+    assert model.generate() == 42
+    assert model.evaluate() == 42
+    print("All tests passed!")
+
+if __name__ == "__main__":
+    test_dummy_model()
